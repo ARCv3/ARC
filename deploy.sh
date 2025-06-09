c469c56f6052dc43ed469cb838479891e6d61b1a
israel.aristide <israel.aristide@plusgrade.com>
2025-06-09 17:50:16 -0400

    export env in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-
+export $(cat .env | xargs)
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-export ARC3_API_VER=`aws ecr describe-images --repository-name arc3-api --query 'sort_by(imageDetails,& imagePushedAt)[-1]' | jq -r '.imageTags[0]'`
-export UNITY_VER=`aws ecr describe-images --repository-name unity --query 'sort_by(imageDetails,& imagePushedAt)[-1]' | jq -r '.imageTags[0]'`
-export ARC3_VER=`aws ecr describe-images --repository-name arc3 --query 'sort_by(imageDetails,& imagePushedAt)[-1]' | jq -r '.imageTags[0]'`
+export ARC3_API_VER=`aws ecr describe-images --repository-name $AWS_ARC3_API_REPO_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1]' | jq -r '.imageTags[0]'`
+export UNITY_VER=`aws ecr describe-images --repository-name $AWS_UNITY_REPO_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1]' | jq -r '.imageTags[0]'`
+export ARC3_VER=`aws ecr describe-images --repository-name $AWS_ARC3_REPO_NAME --query 'sort_by(imageDetails,& imagePushedAt)[-1]' | jq -r '.imageTags[0]'`
 
 docker compose -f docker-compose.yml up
